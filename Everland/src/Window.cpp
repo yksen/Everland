60e7d0c2045afb4d0f56b36ca4a159dc2a5fb1c2
yksen <selegakamil@gmail.com>
2022-05-19 09:29:36 +0200

    Lighting progress

--- Everland/src/Window.cpp
+++ Everland/src/Window.cpp
@@ -23,47 +23,47 @@ namespace Everland
         std::mt19937 mt(time(NULL));
 
         float cubeVertices[] = {
-            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-            0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-            0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-            0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+            0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+            0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+            0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+            -0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
 
-            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-            0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-            0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-            0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-            -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+            -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
 
-            -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-            -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-            -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+            -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+            -0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+            -0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
 
-            0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-            0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-            0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-            0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-            0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-            0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+            0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+            0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+            0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
 
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-            0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-            0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-            0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+            0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+            0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+            0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+            -0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
 
-            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-            0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-            0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-            0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-            -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
+            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+            0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+            0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+            0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+            -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f};
 
         // Time
         float deltaTime = 0.0f;
@@ -78,6 +78,8 @@ namespace Everland
         float lastX = SCR_WIDTH / 2.0f;
         float lastY = SCR_HEIGHT / 2.0f;
         bool firstMouse = true;
+
+        glm::vec3 lightPos(1.0f, 1.0f, 50.0f);
 
         void processInput(GLFWwindow *window)
         {
@@ -209,45 +211,27 @@ namespace Everland
             // Shader Program
             Shader ourShader("../../assets/shaders/vShader.glsl", "../../assets/shaders/fShader.glsl");
 
-            unsigned int VBO, VAO;
-            glGenVertexArrays(1, &VAO);
+            unsigned int VBO, cubeVAO;
+            glGenVertexArrays(1, &cubeVAO);
             glGenBuffers(1, &VBO);
-            glBindVertexArray(VAO);
+            glBindVertexArray(cubeVAO);
             glBindBuffer(GL_ARRAY_BUFFER, VBO);
             glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
             // Position attribute
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
             glEnableVertexAttribArray(0);
-            // Texture coord attribute
-            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+            // Normal attribute
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
             glEnableVertexAttribArray(1);
 
-            /*
-            // Create and load a texture
-            unsigned int texture1;
-            glGenTextures(1, &texture1);
-            glBindTexture(GL_TEXTURE_2D, texture1);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            int width, height, nrChannels;
-            stbi_set_flip_vertically_on_load(true);
-            unsigned char *data = stbi_load("../../assets/images/grass.jpg", &width, &height, &nrChannels, 0);
-            if (data)
-            {
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-                glGenerateMipmap(GL_TEXTURE_2D);
-            }
-            else
-            {
-                std::cout << "Failed to load texture" << std::endl;
-            }
-            stbi_image_free(data);
+            unsigned int lightCubeVAO;
+            glGenVertexArrays(1, &lightCubeVAO);
+            glBindVertexArray(lightCubeVAO);
 
-            ourShader.use();
-            ourShader.setInt("texture1", 0);
-            */
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+            glEnableVertexAttribArray(0);
 
             // Render loop
             while (!glfwWindowShouldClose(Window::window))
@@ -274,6 +258,9 @@ namespace Everland
                 // Shader activation
                 ourShader.use();
 
+                ourShader.setVec3("lightPos", lightPos);
+                ourShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+
                 // Pass projection matrix to shader
                 glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 400.0f);
                 ourShader.setMat4("projection", projection);
@@ -283,7 +270,7 @@ namespace Everland
                 ourShader.setMat4("view", view);
 
                 // Render cubes
-                glBindVertexArray(VAO);
+                glBindVertexArray(cubeVAO);
 
                 for (int x = 0; x < World::world.size(); ++x)
                     for (int z = 0; z < World::world[x].size(); ++z)
@@ -298,9 +285,10 @@ namespace Everland
                             glm::mat4 model = glm::mat4(1.0f);
                             model = glm::translate(model, glm::vec3(-World::worldSize / 2, 0, -World::worldSize / 2));
                             model = glm::translate(model, position);
+                            ourShader.setVec3("objectColor", block.color);
                             ourShader.setMat4("model", model);
-                            ourShader.setVec3("blockColor", block.color);
 
+                            glBindVertexArray(lightCubeVAO);
                             glDrawArrays(GL_TRIANGLES, 0, 36);
                         }
 
@@ -309,7 +297,7 @@ namespace Everland
                 glfwPollEvents();
             }
 
-            glDeleteVertexArrays(1, &VAO);
+            glDeleteVertexArrays(1, &cubeVAO);
             glDeleteBuffers(1, &VBO);
         }
     }
--- assets/shaders/fShader.glsl
+++ assets/shaders/fShader.glsl
@@ -3,13 +3,26 @@ out vec4 FragColor;
 
 in vec2 TexCoord;
 in vec3 FragPos;
+in vec3 Normal;
 
 // texture samplers
 uniform sampler2D texture1;
-uniform vec3 blockColor;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;  
 
 void main()
 {
-	// FragColor = texture(texture1, TexCoord);
-	FragColor = vec4(blockColor, 1.0);
+	// ambient
+    float ambientStrength = 1;
+    vec3 ambient = ambientStrength * lightColor;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+        
+    vec3 result = (ambient + diffuse) * objectColor;
+    FragColor = vec4(result, 1.0);
 }
